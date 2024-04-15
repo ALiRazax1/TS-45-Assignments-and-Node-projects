d5c012cebabae501c95e0a2c2880cee8233c549c
Ali Raza <161970819+ALiRazax1@users.noreply.github.com>
2024-04-16 00:35:28 +0500

    Delete Typescript 45 Assignments/Exercise_40 directory

--- Typescript 45 Assignments/Exercise_40/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function make_album(artist, title) {
-    return { artist,
-        title };
-}
-console.log(make_album("Artist1", "Album1"));
-console.log(make_album("Artist2", "Album2"));
-console.log(make_album("Artist3", "Album3"));
