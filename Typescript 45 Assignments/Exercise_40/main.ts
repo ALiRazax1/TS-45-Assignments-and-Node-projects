9bae789c8386697a4d99c68bf3bfbcc8623c3c7e
Ali Raza <ali.raza95123@gmail.com>
2024-04-16 23:01:13 +0500

    Upload Assignments

--- Typescript 45 Assignments/Exercise_40/main.ts
+++ Typescript 45 Assignments/Exercise_40/main.ts
@@ -1,11 +1,12 @@
 function make_album(artist: string, title: string, tracks?: number) {
-  if (tracks) {
-    return { artist, title, tracks };
-  } else {
-    return { artist, title };
+    if (tracks) {
+      return { artist, title, tracks };
+    } else {
+      return { artist, title };
+    }
   }
-}
-
-console.log(make_album("Artist1", "Title1"));
-console.log(make_album("Artist2", "Title2"));
-console.log(make_album("Artist3", "Title3", 5));
+  
+  console.log(make_album("Artist1", "Title1"));
+  console.log(make_album("Artist2", "Title2"));
+  console.log(make_album("Artist3", "Title3", 5));
+  
